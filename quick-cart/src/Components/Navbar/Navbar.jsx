b0ab0ac7e4326d4c0aacf41e6c709cf45d909279
roshan Kumar <roshan.kmr8309@gmail.com>
2024-07-24 20:33:04 +0530

    working on Navbar

--- quick-cart/src/Components/Navbar/Navbar.jsx
+++ quick-cart/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart from '../Assets/cart.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="" id='logo'/>
+        <p>Quick Cart</p>
+      </div>
+      <ul className="tabs">
+        <li>Basics</li>
+        <li>Apperals</li>
+        <li>Male</li>
+        <li>Female</li>
+        <li>About US</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button name='Login'></button>
+        <img src={cart} alt="" />
+      </div>
     </div>
   )
 }
