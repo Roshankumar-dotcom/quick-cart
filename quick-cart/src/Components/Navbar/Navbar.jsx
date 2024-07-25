3e8fa79a304d37a45a8286bc1331981200a89310
roshan Kumar <roshan.kmr8309@gmail.com>
2024-07-25 17:46:47 +0530

    Created navbar

--- quick-cart/src/Components/Navbar/Navbar.jsx
+++ quick-cart/src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart from '../Assets/cart.png'
 
 const Navbar = () => {
+  const [tab,setTab] = useState("")
   return (
     <div className="navbar">
       <div className="logo">
-        <img src={logo} alt="" id='logo'/>
+        <img src={logo} alt=""/>
         <p>Quick Cart</p>
       </div>
       <ul className="tabs">
-        <li>Basics</li>
-        <li>Apperals</li>
-        <li>Male</li>
-        <li>Female</li>
-        <li>About US</li>
+        <li onClick={()=>{setTab("grocery")}}>Grocery{ tab === 'grocery'? <hr/>:<></>}</li>
+        <li onClick={()=>{setTab("electronics")}}>Electronics { tab === 'electronics'? <hr/>:<></>}</li>
+        <li onClick={()=>{setTab("mens")}}>Men { tab === 'mens'? <hr/>:<></>}</li>
+        <li onClick={()=>{setTab("womens")}}>Women { tab === 'womens'? <hr/>:<></>}</li>
+        <li  onClick={()=>{setTab("aboutus")}}>About US  { tab === 'aboutus'? <hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <button name='Login'></button>
-        <img src={cart} alt="" />
+        <button name='Login'>Login</button>
+        <img src={cart} alt=""/>
+        <div className="items-counter">
+          0
+        </div>
       </div>
     </div>
   )
